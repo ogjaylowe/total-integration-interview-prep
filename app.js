3fe635baaaf86eab376d452e3c420a12d03a0ffa
Jay Lowe <jaylowe@news-mbp.lan>
2021-04-12 07:18:28 -0700

    forgot process env port

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.get('/twopointersCompanion', (req, res) => {
 })
 
 // Start Server
-
-app.listen(3000, () => {
+const port = process.env.PORT || 3000
+app.listen(port, () => {
     console.log('server listening on port 3000');
 });
